255af9f8f699740159ae370e77c4b9acfb891ca8
Samuel Pritchard <sampritchard0801@gmail.com>
2020-01-10 01:15:49 +0000

    add dependencies and a base

--- build.sbt
+++ build.sbt
@@ -1,10 +1,16 @@
 lazy val kunou = (project in file("."))
   .enablePlugins(UniversalPlugin)
   .settings(
-    organization := "io.github.samophis",
-    name := "Kunou",
-    scalaVersion := "2.13.1",
-    version := "0.1.0",
-    resolvers += Resolver.jcenterRepo,
-    libraryDependencies += "net.katsstuff" %% "ackcord" % "0.15.0"
+      organization := "io.github.samophis",
+      name := "Kunou",
+      scalaVersion := "2.13.1",
+      version := "0.1.0",
+      resolvers += Resolver.jcenterRepo,
+      resolvers += "jitpack" at "https://jitpack.io",
+
+      libraryDependencies ++= Seq(
+        "com.mewna" % "catnip" % "1b5e2a7",
+        "io.sentry" % "sentry-logback" % "1.7.29",
+        "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+      )
   )
--- src/main/scala/io/github/samophis/kunou/startup/Kunou.scala
+++ src/main/scala/io/github/samophis/kunou/startup/Kunou.scala
@@ -1,5 +1,35 @@
 package io.github.samophis.kunou.startup
 
-class Kunou {
+import java.util
 
+import com.mewna.catnip.cache.CacheFlag
+import com.mewna.catnip.{Catnip, CatnipOptions}
+import com.typesafe.scalalogging.Logger
+import io.sentry.Sentry
+
+class Kunou {
+  private[this] val token = sys.env("KUNOU_BOT_TOKEN")
+  private[this] val logger = Logger[Kunou]
+
+  val ownerId = sys.env("KUNOU_OWNER_ID").toLong
+  val defaultCommandPrefix = sys.env.get("KUNOU_DEFAULT_PREFIX") match {
+    case None => "k."
+    case Some(default) => default
+  }
+
+  private[this] val catnipOptions = new CatnipOptions(token)
+    .cacheFlags(util.Set.of(CacheFlag.DROP_GAME_STATUSES, CacheFlag.DROP_EMOJI))
+    .memberChunkTimeout(5000)
+    .validateToken(true)
+  val catnip = Catnip.catnip(catnipOptions).connect()
+
+  sys.env.get("KUNOU_SENTRY_DSN") match {
+    case Some(sentryDsn) =>
+      Sentry.init(sentryDsn)
+      logger.info("Sentry Client initialized! Errors and warnings will be printed to the console and sent to Sentry.")
+    case None =>
+      logger.warn("No Sentry DSN specified. Errors and warnings - like this one - will only be tracked in the console.")
+  }
+
+  logger.info("Connected to Discord!")
 }
