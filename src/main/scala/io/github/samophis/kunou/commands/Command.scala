f7d1a11b0333c0bd542107fc531e4fb72ce71870
Samuel Pritchard <sampritchard0801@gmail.com>
2020-03-12 18:28:31 +0000

    Add comment explaining why one field is lazily-initialized.

--- src/main/scala/io/github/samophis/kunou/commands/Command.scala
+++ src/main/scala/io/github/samophis/kunou/commands/Command.scala
@@ -8,8 +8,8 @@ import io.github.samophis.kunou.startup.Kunou
 trait Command {
   val name: String
   // This field is lazy as some commands (read: EmoteCommand as of 2020/03/12) rely on initially null values.
-  // By the time this field is used, the initially null value is initialized.
   lazy val description: Option[String] = None
+
   val usage: String = name
   // These permission lists are lists, not sets, so that we can inter-op with Catnip seamlessly.
   // There are probably hacks to convert a Scala set to Java var-args, but I am not aware of such things at the moment.
