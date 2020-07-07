5aaf0e9815227aa06c9fe7b7b6e89b3dcfcc9245
Sam Pritchard <ophisbonklol@gmail.com>
2020-07-07 20:17:57 +0100

    Remove error format test.

--- bot/src/kunou/startup/Main.scala
+++ bot/src/kunou/startup/Main.scala
@@ -6,7 +6,6 @@ import com.typesafe.scalalogging.Logger
 
 
 object Main extends App {
-  Logger("ErrorTest").error("An error occurred while starting up!", new RuntimeException)
   val discordClientSettings = ClientSettings(sys.env("KUNOU_BOT_TOKEN"))
   val redisClient = new RedisClient("localhost", 6379)
 
