6769c9bf3c92c88121a500c4046f6f7872958120
Sam Pritchard <ophisbonklol@gmail.com>
2020-07-13 02:26:19 +0100

    Reflect constructor passing changes, and introduce command listing.

--- bot/src/kunou/commands/list/general/package.scala
+++ bot/src/kunou/commands/list/general/package.scala
@@ -1,43 +1,105 @@
 package kunou.commands.list
 
 import ackcord.CacheSnapshot
+import ackcord.data.OutgoingEmbed
 import ackcord.requests.GetCurrentUser
 import com.typesafe.scalalogging.Logger
-import kunou.commands.{Command, Context}
+import kunou.commands.Category.Category
+import kunou.commands.{Category, Command, Context}
+import kunou.startup.Kunou
 import kunou.syntax._
 
+import scala.collection.mutable
 import scala.util.control.NonFatal
 import scala.util.{Failure, Success}
 
 package object general {
-  class PingCommand extends Command {
+  class PingCommand(bot: Kunou) extends Command {
+    import bot.discordClient.requestsHelper.run
+    import bot.clientSettings.executionContext
+
     private val logger = Logger[PingCommand]
     override def name: String = "ping"
 
     override def execute(implicit context: Context): Unit = context match {
-      case Context(bot, msg, _, _ ,_ ) =>
-        import bot.discordClient.requestsHelper.run
-        import bot.clientSettings.executionContext
-
-        implicit val c: CacheSnapshot = context.cacheSnapshot
-        val channel = msg.channelId.resolve.get
+      case Context(_, msg, _, _ ,_ ) =>
 
         // We don't care about the actual user from the request, so we convert back to Future[Option[User]]
         // - so that we can just run Future#onComplete.
         // Used to obtain network latency (for the REST API).
 
+        implicit val c: CacheSnapshot = context.cacheSnapshot
         val firstTimeNanos = System.nanoTime()
+
         run(GetCurrentUser).value.onComplete {
           case Success(_) =>
             val secondTimeNanos = System.nanoTime()
             val apparantLatency = (secondTimeNanos - firstTimeNanos) / 1000000d
             val formattedLatencyString = f"$apparantLatency%.2fms"
-            run(channel.sendEmbed(info"🏓 Discord API Latency: **$formattedLatencyString**"))
+            run(msg.channelId.sendEmbed(info"🏓 Discord API Latency: **$formattedLatencyString**"))
 
           case Failure(NonFatal(error)) =>
             logger.error(s"An error occurred while making a request to the Discord API.", error)
-            run(channel.sendEmbed(error"An error occurred. Sorry about that."))
+            run(msg.channelId.sendEmbed(error"An error occurred. Sorry about that."))
         }
+    }
+  }
+
+  class CommandListCommand(bot: Kunou) extends Command {
+    import bot.discordClient.requestsHelper.run
+    import scala.collection.{Set => SetParent}
+
+    private val categoryCommandSetMap: mutable.Map[Category, mutable.Buffer[Command]] = mutable.Map()
+
+    override def name: String = "commandlist"
+
+    override def aliases: Set[String] = Set("cmdlist", "list")
+
+    override def execute(implicit context: Context): Unit = {
+      implicit val c: CacheSnapshot = context.cacheSnapshot
+
+      if (categoryCommandSetMap.isEmpty) {
+        bot.commandManager.commandMap.values.foreach(command => {
+          categoryCommandSetMap.get(command.category) match {
+            case Some(buffer) => buffer += command
+            case _ =>
+              val newBuffer = mutable.Buffer[Command]()
+              newBuffer += command
+              categoryCommandSetMap.put(command.category, newBuffer)
+          }
+        })
+      }
+
+      def generateEmbed(categorySet: SetParent[Category]): OutgoingEmbed = if (categorySet.nonEmpty) {
+        val stringBuilder = new mutable.StringBuilder
+        categorySet.foreach(category => {
+          val commandBuffer = categoryCommandSetMap(category)
+          stringBuilder ++= "**"
+          stringBuilder ++= category.toString
+          stringBuilder ++= "**: "
+          stringBuilder ++= commandBuffer.map(command => s"${context.prefix}${command.name}").mkString(", ")
+          stringBuilder ++= "\n"
+        })
+        info"${stringBuilder.toString}"
+      } else {
+        val categoryList = categoryCommandSetMap.keySet.map(_.toString).mkString(", ")
+        warn"No valid command categories were specified. Pick from this list: $categoryList."
+      }
+
+      def argumentToCategory(argument: String): Option[Category] =
+        Category.values.find(_.toString.equalsIgnoreCase(argument))
+
+      val createMessage = context match {
+        case Context(_, msg, _, List(), _) =>
+          val embed = generateEmbed(categoryCommandSetMap.keySet)
+          msg.channelId.sendEmbed(embed)
+
+        case Context(_, msg, _, args, _) =>
+          val embed = generateEmbed(args.flatMap(argumentToCategory).toSet)
+          msg.channelId.sendEmbed(embed)
+      }
+
+      run(createMessage)
     }
   }
 }
